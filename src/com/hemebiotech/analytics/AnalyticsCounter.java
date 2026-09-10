58f1fccd9b94a44a0a1aa330a933c68874f66e29
Just1Fr <190003579+Just1Fr@users.noreply.github.com>
2026-09-10 17:31:27 +0200

    refactor: add constructor and implement countSymptoms in AnalyticsCounter

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,16 +8,18 @@ public class AnalyticsCounter {
 	private static int rashCount = 0;
 	private static int pupilCount = 0;
 
-	public static void main(String args[]) throws Exception {
-		String symptomsFilePath = "data/symptoms.txt";
-		String resultFilePath = "data/result.out";
+	private ISymptomReader reader;
+    private ISymptomWriter writer;
 
-		ReadSymptomDataFromFile reader = new ReadSymptomDataFromFile(symptomsFilePath);
+	public AnalyticsCounter(ISymptomReader reader, ISymptomWriter writer) {
+        this.reader = reader;
+        this.writer = writer;
+    }
 
-		List<String> symptoms = reader.getSymptoms();
-
+	public Map<String, Integer> countSymptoms(List<String> symptoms) {
+		System.out.println("- Symptoms from file:");
 		for (String symptom : symptoms) {
-			System.out.println("symptom from file: " + symptom);
+			System.out.println(symptom);
 			if (symptom.equals("headache")) {
 				headacheCount++;
 			}
@@ -35,7 +37,22 @@ public class AnalyticsCounter {
 			"rash", rashCount
 		);
 
-		WriteSymptomDataToFile writer = new WriteSymptomDataToFile(resultFilePath);
-		writer.writeSymptoms(result);
+		System.out.println("- Result: " + result);
+
+		return result;
+	}
+
+	public static void main(String args[]) throws Exception {
+		String symptomsFilePath = "data/symptoms.txt";
+		String resultFilePath = "data/result.out";
+
+		AnalyticsCounter analytics = new AnalyticsCounter(
+			new ReadSymptomDataFromFile(symptomsFilePath),
+			new WriteSymptomDataToFile(resultFilePath)
+		);
+
+		List<String> symptoms = analytics.reader.getSymptoms();
+		Map<String, Integer> symptomCounts = analytics.countSymptoms(symptoms);
+		analytics.writer.writeSymptoms(symptomCounts);
 	}
 }
